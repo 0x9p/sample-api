701714a579799362f7c1da910836f0a244a241ee
Alexey Bedonik <alexey.bedonik@gmail.com>
2023-10-26 12:49:33 +0200

    first commit

---
+++ SimpleAPI.Tests/WeatherForecastControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Tests
+{
+    public class WeatherForecastControllerTests
+    {
+        [Fact]
+        public void Get_ReturnsFiveWeatherForecasts()
+        {
+            // Arrange
+            var controller = new WeatherForecastController(new Logger<WeatherForecastController>(new LoggerFactory()));
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            var weatherForecasts = Assert.IsType<List<WeatherForecast>>(result.ToList());
+            Assert.Equal(5, weatherForecasts.Count);
+        }
+    }
+}
